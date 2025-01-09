75248f75ff3def19744241acb9cffd730587c0af
wkemrm <63217462+wkemrm@users.noreply.github.com>
2025-01-09 21:37:04 +0900

    [Silver I] Title: 스타트와 링크, Time: 364 ms, Memory: 18812 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -3,54 +3,53 @@ import java.io.*;
 
 class Main {
     static int n;
-    static int[][] graph;
     static boolean[] visited;
+    static int[][] graph;
     static int result = Integer.MAX_VALUE;
 
-    public static void diff() {
-        int startSum = 0;
-        int linkSum = 0;
-        for (int i = 1 ; i < n ; i++) {
-            for (int j = i + 1 ; j <= n ; j++) {
-                if (visited[i] == true && visited[j] == true) {
-                    startSum += graph[i][j] + graph[j][i];
-                } else if (visited[i] == false && visited[j] == false) {
-                    linkSum += graph[i][j] + graph[j][i];
-                }
-            }
-        }
-
-        result = Math.min(result, Math.abs(startSum - linkSum));
-    }
-
-    public static void combi(int idx, int count) {
-        if (count == n / 2) {
+    public static void combi(int depth, int before) {
+        if (depth == n / 2) {
             diff();
-            return;
         } else {
-            for (int i = idx ; i <= n ; i++) {
+            for (int i = before ; i <= n ; i++) {
                 visited[i] = true;
-                combi(i + 1, count + 1);
+                combi(depth + 1, i + 1);
                 visited[i] = false;
             }
         }
     }
 
+    public static void diff() {
+        int startSum = 0;
+        int linkSum = 0;
+
+        for (int i = 1 ; i < n ; i++) {
+            for (int j = i + 1 ; j <= n ; j++) {
+                if (visited[i] == true && visited[j] == true) {
+                    startSum += graph[i - 1][j - 1] + graph[j - 1][i - 1];
+                } else if (visited[i] == false && visited[j] == false) {
+                    linkSum += graph[i - 1][j - 1] + graph[j - 1][i - 1];
+                }
+            }
+        }
+        result = Math.min(result, Math.abs(startSum - linkSum));
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        graph = new int[n + 1][n + 1];
+
+        graph = new int[n][n];
         visited = new boolean[n + 1];
 
-        for (int i = 1 ; i <= n ; i++) {
+        for (int i = 0 ; i < n ; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1 ; j <= n ; j++) {
+            for (int j = 0 ; j < n ; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        combi(1, 0);
-
+        combi(0, 1);
         System.out.print(result);
     }
 }
