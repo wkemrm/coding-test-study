c64b01cfd28a918dda7ac75e16cca34beee3cecb
wkemrm <63217462+wkemrm@users.noreply.github.com>
2025-01-09 21:02:32 +0900

    [Gold IV] Title: 스도쿠, Time: 468 ms, Memory: 26628 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -2,49 +2,48 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int[][] graph = new int[10][10];
+    static int[][] graph = new int[9][9];
 
-    public static void solution(int x, int y) {
-        if (y == 10) {
-            solution(x + 1, 1);
+    public static void sdoku(int x, int y) {
+        if (y == 9) {
+            sdoku(x + 1, 0);
             return;
         }
 
-        if (x == 10) {
-            for (int i = 1 ; i <= 9 ; i++) {
-                for (int j = 1 ; j < 9 ; j++) {
+        if (x == 9) {
+            for (int i = 0 ; i < 9 ; i++) {
+                for (int j = 0 ; j < 9 ; j++) {
                     System.out.print(graph[i][j] + " ");
                 }
-                System.out.println(graph[i][9]);
+                System.out.println();
             }
             System.exit(0);
+            return;
         }
 
         if (graph[x][y] == 0) {
             for (int i = 1 ; i <= 9 ; i++) {
                 if (possible(x, y, i)) {
                     graph[x][y] = i;
-                    solution(x, y + 1);
+                    sdoku(x, y + 1);
                 }
             }
             graph[x][y] = 0;
             return;
         }
-        solution(x, y + 1);
+
+        sdoku(x, y + 1);
     }
 
-    public static boolean possible(int x, int y, int i) {
-        // 가로 확인
+    public static boolean possible(int x, int y , int i) {
         if (!garo(x, i)) {
             return false;
         }
 
-        // 세로 확인
         if (!sero(y, i)) {
             return false;
         }
 
-        // 블록 확인
         if (!block(x, y, i)) {
             return false;
         }
@@ -53,32 +52,26 @@ class Main {
     }
 
     public static boolean garo(int x, int input) {
-        for (int i = 1 ; i <= 9 ; i++) {
-            if (graph[x][i] == input) {
-                return false;
-            }
+        for (int i = 0 ; i < 9 ; i++) {
+            if (graph[x][i] == input) return false;
         }
-
         return true;
     }
 
     public static boolean sero(int y, int input) {
-        for (int i = 1 ; i <= 9 ; i++) {
-            if (graph[i][y] == input) {
-                return false;
-            }
+        for (int i = 0 ; i < 9 ; i++) {
+            if (graph[i][y] == input) return false;
         }
-
         return true;
     }
 
     public static boolean block(int x, int y, int input) {
-        int startX = ((x - 1) / 3) * 3 + 1;
-        int startY = ((y - 1) / 3) * 3 + 1;
+        int startX = (x / 3) * 3;
+        int startY = (y / 3) * 3;
 
         for (int i = startX ; i < startX + 3 ; i++) {
             for (int j = startY ; j < startY + 3 ; j++) {
-                if(graph[i][j] == input) return false;
+                if (graph[i][j] == input) return false;
             }
         }
 
@@ -87,15 +80,13 @@ class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for (int i = 1 ; i < 10 ; i++) {
+        for (int i = 0 ; i < 9 ; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1 ; j < 10 ; j++) {
-                int input = Integer.parseInt(st.nextToken());
-                graph[i][j] = input;
+            for(int j = 0 ; j < 9 ; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        solution(1, 1);
 
+        sdoku(0, 0);
     }
 }
