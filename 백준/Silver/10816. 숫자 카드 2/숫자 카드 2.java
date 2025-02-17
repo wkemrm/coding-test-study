80c2463840115c0672463213d869f9b57f50dd29
wkemrm <63217462+wkemrm@users.noreply.github.com>
2025-02-17 10:07:00 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1452 ms, Memory: 137800 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,84 +1,60 @@
-import java.util.Arrays;
-import java.util.StringTokenizer;
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		int[] arr = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			arr[i] = in.nextInt();
-		}
-		
-		Arrays.sort(arr);	// 이분 탐색을 하기 위해서는 반드시 정렬되어있어야 한다.
-		
-		int M = in.nextInt();
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0; i < M; i++) {
-			int key = in.nextInt();
- 
-			// upperBound와 lowerBound의 차이 값을 구한다.
-			sb.append(upperBound(arr, key) - lowerBound(arr, key)).append(' ');
-		}
-		System.out.println(sb);
-	}
- 
- 
-	private static int lowerBound(int[] arr, int key) {
-		int lo = 0; 
-		int hi = arr.length; 
- 
-		// lo가 hi랑 같아질 때 까지 반복
-		while (lo < hi) {
- 
-			int mid = (lo + hi) / 2; // 중간위치를 구한다.
- 
-			/*
-			 *  key 값이 중간 위치의 값보다 작거나 같을 경우
-			 *  
-			 *  (중복 원소에 대해 왼쪽으로 탐색하도록 상계를 내린다.)
-			 */
-			if (key <= arr[mid]) {
-				hi = mid;
-			}
- 
-			else {
-				lo = mid + 1;
-			}
- 
-		}
- 
-		return lo;
-	}
- 
-	private static int upperBound(int[] arr, int key) {
-		int lo = 0; 
-		int hi = arr.length; 
- 
-		// lo가 hi랑 같아질 때 까지 반복
-		while (lo < hi) {
- 
-			int mid = (lo + hi) / 2; // 중간위치를 구한다.
- 
-			// key값이 중간 위치의 값보다 작을 경우
-			if (key < arr[mid]) {
-				hi = mid;
-			}
-			// 중복원소의 경우 else에서 처리된다.
-			else {
-				lo = mid + 1;
-			}
- 
-		}
- 
-		return lo;
-	}
-	
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr1 = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < n ; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+        int m = Integer.parseInt(br.readLine());
+        int[] arr2 = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < m ; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ; i < m ; i++) {
+            int find = arr2[i];
+
+            int lt = 0;
+            int rt = n;
+            while(lt < rt) {
+                int mid = (lt + rt) / 2;
+
+
+                if (arr1[mid] >= find) {
+                    rt = mid;
+                } else {
+                    lt = mid + 1;
+                }
+            }
+            int start = lt;
+
+            lt = 0;
+            rt = n;
+
+            while(lt < rt) {
+                int mid = (lt + rt) / 2;
+
+                if (arr1[mid] > find) {
+                    rt = mid;
+                } else {
+                    lt = mid + 1;
+                }
+            }
+
+            int end = lt;
+
+            sb.append(end - start + " ");
+        }
+
+        System.out.println(sb.toString());
+    }
 }
