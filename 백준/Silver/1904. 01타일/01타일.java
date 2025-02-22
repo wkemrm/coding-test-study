0b5853320e0928cb935b83a99f7558a66bf4afe3
wkemrm <63217462+wkemrm@users.noreply.github.com>
2025-02-22 13:57:12 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 18160 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,33 +1,22 @@
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static int[] dp = new int[1000001];;
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
-		
-		dp[0] = 0;
-		dp[1] = 1;
-		dp[2] = 2;
- 
-		// -1 로 초기화
-		for(int i = 3; i < dp.length; i++) {
-			dp[i] = -1;
-		}
-		
-		System.out.println(Tile(N));
-		
-	}
-	
-	public static int Tile(int N) {
-		
-		if(dp[N] == -1) {
-			dp[N] = (Tile(N - 1) + Tile((N - 2))) % 15746;
-		}
-		return dp[N];
-	}
- 
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n == 1) {
+            System.out.print(1);
+            return;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3 ; i <= n ; i++) {
+            dp[i] = (dp[i - 1] % 15746 + dp[i - 2] % 15746) % 15746;
+        }
+        
+        System.out.print(dp[n]);
+    }
 }
