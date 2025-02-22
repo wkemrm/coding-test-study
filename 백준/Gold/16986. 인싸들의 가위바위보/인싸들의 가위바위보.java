07fb9201dd66a62c99c83f33678671f7b2d7cfe1
wkemrm <63217462+wkemrm@users.noreply.github.com>
2025-02-22 15:33:54 +0900

    [Gold II] Title: 인싸들의 가위바위보, Time: 272 ms, Memory: 40364 KB -BaekjoonHub

--- 백준/Gold/16986. 인싸들의 가위바위보/인싸들의 가위바위보.java
+++ 백준/Gold/16986. 인싸들의 가위바위보/인싸들의 가위바위보.java
@@ -1,104 +1,100 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	static int N, K, map[][], round[][], win[];
-	static boolean visit[], flag;
+class Main {
+    static int n;
+    static int k;
+    static int[][] graph;
+    static int[][] command;
+    static boolean[] visited;
+    static boolean flag;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine());
-		N = Integer.valueOf(st.nextToken());
-		K = Integer.valueOf(st.nextToken());
-		map = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(in.readLine());
-			for (int j = 1; j <= N; j++) {
-				map[i][j] = Integer.valueOf(st.nextToken());
-			}
-		}
-		flag = false;
-		win = new int[4];
-		visit = new boolean[N + 1];
-		round = new int[4][21];
-		for (int i = 2; i <= 3; i++) {
-			st = new StringTokenizer(in.readLine());
-			for (int j = 1; j <= 20; j++) {
-				round[i][j] = Integer.valueOf(st.nextToken());
-			}
-		}
-		perm(1);
-		if (flag)
-			System.out.println(1);
-		else
-			System.out.println(0);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        graph = new int[n + 1][n + 1];
+        command = new int[4][21];
+        visited = new boolean[n + 1];
 
-	static void perm(int dep) {
-		if (flag) {
-			return;
-		}
-		if (dep == N + 1) {
-			game();
-			return;
-		}
-		for (int i = 1; i <= N; i++) {
-			if (!visit[i]) {
-				visit[i] = true;
-				round[1][dep] = i;
-				perm(dep + 1);
-				visit[i] = false;
-			}
-		}
-	}
+        for (int i = 1 ; i <= n ; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1 ; j <= n ; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-	static void game() {
-		int index[] = new int[4];
-		for(int i=1; i<=3; i++)
-			index[i]=1;
-		int p1 = 1;
-		int p2 = 2;
-		int np = 3;
-		int win[] = new int[4];
-		while (true) {
-			np = 6 - (p1 + p2);
-			if (win[1] == K) {
-				flag = true;
-				return;
-			}
-			if (win[2] == K || win[3] == K) {
-				return;
-			}
-			if (index[1] == N + 1) {
-				return;
-			}
-			if (index[2] == 21 || index[3]==21) {
-				return;
-			}
+        for (int i = 2 ; i <= 3 ; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1 ; j <= 20 ; j++) {
+                command[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-			int cmd1 = round[p1][index[p1]];
-			int cmd2 = round[p2][index[p2]];
-			int winner = winCheck(cmd1, cmd2, p1, p2);
-			win[winner]++;
-			index[p1]++;
-			index[p2]++;
-			p1 = winner;
-			p2 = np;
+        DFS(1);
 
-		}
-	}
+        System.out.print(flag ? 1 : 0);
+    }
 
-	static int winCheck(int cmd1, int cmd2, int p1, int p2) {
-		if (map[cmd1][cmd2] == 2) {
-			return p1;
-		} else if (map[cmd1][cmd2] == 1) {
-			return Math.max(p1, p2);
-		} else {
-			return p2;
-		}
-	}
+    public static void gameStart() {
+        int[] winCount = new int[4];
+        int[] now = new int[4];
+        Arrays.fill(now, 1);
+        int player1 = 1;
+        int player2 = 2;
+        int nextPlayer = 3;
+        while(true) {
+            if (winCount[1] == k) {
+                flag = true;
+                break;
+            }
+
+            if (winCount[2] == k || winCount[3] == k) {
+                break;
+            }
+
+            if (now[1] == n + 1 || now[2] == 21 || now[3] == 21) {
+                break;
+            }
+
+            nextPlayer = 6 - player1 - player2;
+
+            int winner = win(player1, player2, now);
+            winCount[winner]++;
+            now[player1]++;
+            now[player2]++;
+
+            player1 = winner;
+            player2 = nextPlayer;
+        }
+    }
+
+    public static int win(int player1, int player2, int[] now) {
+        if (graph[command[player1][now[player1]]][command[player2][now[player2]]] == 2) {
+            return player1;
+        } else if (graph[command[player1][now[player1]]][command[player2][now[player2]]] == 0) {
+            return player2;
+        } else {
+            return Math.max(player1, player2);
+        }
+    }
+
+    public static void DFS(int depth) {
+        if (depth == n + 1) {
+            if(!flag) {
+                gameStart();
+            }
+        } else {
+            for (int i = 1 ; i <= n ; i++) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    command[1][depth] = i;
+                    DFS(depth + 1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+
 }
