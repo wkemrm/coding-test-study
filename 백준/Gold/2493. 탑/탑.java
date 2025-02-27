4120368c2f33f658c3224106d35c89c0dd6ff60b
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-02-27 15:29:50 +0900

    [Gold V] Title: 탑, Time: 2524 ms, Memory: 132956 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -3,46 +3,34 @@ import java.io.*;
 
 class Top {
     int index;
-    int height;
-    public Top (int index, int height) {
+    int length;
+    
+    public Top(int index, int length) {
         this.index = index;
-        this.height = height;
+        this.length = length;
     }
 }
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Stack<Top> top = new Stack<>();
+        int[] result = new int[n + 1];
+        Stack<Top> stack = new Stack<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0 ; i < n ; i++) {
+        for (int i = 1 ; i <= n ; i++) {
             int input = Integer.parseInt(st.nextToken());
-
-            if (top.isEmpty()) {
-                System.out.print(0 + " ");
-                top.push(new Top(i + 1, input));
-            } else {
-                boolean flag = false;
-                int index = 0;
-                while(!top.isEmpty() && !flag) {
-                    int before = top.peek().height;
-                    if (before < input) {
-                        top.pop();
-                    } else {
-                        flag = true;
-                        index = top.peek().index;
-                        break;
-                    }
+            while(!stack.isEmpty()) {
+                if (stack.peek().length > input) {
+                    result[i] = stack.peek().index;
+                    break;
                 }
-
-                if (!flag) {
-                    System.out.print(0 + " ");
-                } else {
-                    System.out.print(index + " ");
-                }
-
-                top.push(new Top(i + 1, input));
+                stack.pop();
             }
+            stack.push(new Top(i, input));
+        }
+        
+        for (int i = 1 ; i <= n ; i++) {
+            System.out.print(result[i] + " ");
         }
     }
 }
