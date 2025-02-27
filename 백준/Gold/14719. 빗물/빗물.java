29cad5dbddf185d285c8732cf656feb33f2555b6
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-02-27 19:21:22 +0900

    [Gold V] Title: 빗물, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -7,22 +7,25 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int h = Integer.parseInt(st.nextToken());
         int w = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
+        
         int[] height = new int[w];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0 ; i < w ; i++) {
             height[i] = Integer.parseInt(st.nextToken());
         }
-        int answer = 0;
         
+        int answer = 0;
         for (int i = 1 ; i < w ; i++) {
             int left = 0;
             for (int j = i - 1 ; j >= 0 ; j--) {
                 left = Math.max(left, height[j]);
             }
+            
             int right = 0;
-            for (int j= i + 1 ; j < w ; j++) {
+            for (int j = i + 1 ; j < w ; j++) {
                 right = Math.max(right, height[j]);
             }
+            
             if (left > height[i] && right > height[i]) {
                 answer += (Math.min(left, right) - height[i]);
             }
