665d5b079ec1c81e57176166cbe8a523727edc95
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-02-28 15:36:17 +0900

    [Gold V] Title: 톱니바퀴, Time: 104 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -1,136 +1,104 @@
 import java.util.*;
 import java.io.*;
 
-class Circle {
-    int index;
-    List<Integer> statusList = new ArrayList<>();
-
-    public Circle(int index) {
-        this.index = index;
-    }
-
-    public void left() {
-        statusList.add(statusList.remove(0));
-    }
-
-    public void right() {
-        statusList.add(0, statusList.remove(statusList.size() - 1));
-    }
-
-    // 만약 극이 같은 경우 true
-    public boolean leftCouple(Circle circle) {
-        return statusList.get(6).equals(circle.statusList.get(2));
-    }
-
-    // 만약 극이 같은 경우 true
-    public boolean rightCouple(Circle circle) {
-        return statusList.get(2).equals(circle.statusList.get(6));
-    }
-
-    public int getResult() {
-        if (index == 1) {
-            return statusList.get(0).equals(1) ? 1 : 0;
-        } else if (index == 2) {
-            return statusList.get(0).equals(1) ? 2 : 0;
-        } else if (index == 3) {
-            return statusList.get(0).equals(1) ? 4 : 0;
-        } else if (index == 4) {
-            return statusList.get(0).equals(1) ? 8 : 0;
-        } else {
-            return 0;
-        }
-    }
-
-}
-
-class Total {
-    Circle[] circles;
-
-    public Total(Circle[] circles) {
-        this.circles = circles;
-    }
-
-    public int[] direction(int index, int direction) {
-        int[] result = new int[5];
-        result[index] = direction;
-        // 좌측
-        for (int i = index - 1 ; i > 0 ; i--) {
-            if (circles[i].rightCouple(circles[i + 1])) {
-                break;
-            } else {
-                if (direction == -1) {
-                    result[i] = 1;
-                    direction = 1;
-                } else {
-                    result[i] = -1;
-                    direction = -1;
-                }
-            }
-        }
-        direction = result[index];
-        // 우측
-        for (int i = index + 1 ; i <= 4 ; i++) {
-            if (circles[i].leftCouple(circles[i - 1])) {
-                break;
-            } else {
-                if (direction == -1) {
-                    result[i] = 1;
-                    direction = 1;
-                } else {
-                    result[i] = -1;
-                    direction = -1;
-                }
-            }
-        }
-
-        return result;
-    }
-
-    public void rotation(int index, int direction) {
-        int[] directionArr = this.direction(index, direction);
-
-        for (int i = 1 ; i <= 4 ; i++) {
-            if (directionArr[i] == -1) {
-                circles[i].left();
-            } else if(directionArr[i] == 1) {
-                circles[i].right();
-            }
-        }
-    }
-
-    public int result() {
-        int sum = 0;
-        for (int i = 1 ; i <= 4 ; i++) {
-            sum += circles[i].getResult();
-        }
-        return sum;
-    }
-}
-
 class Main {
-    static Circle[] arr = new Circle[5];
-
+    static Node[] nodeArr = new Node[5];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 1 ; i <= 4 ; i++) {
-            arr[i] = new Circle(i);
-            char[] cArr = br.readLine().toCharArray();
-            for (int j = 0 ; j < cArr.length ; j++) {
-                arr[i].statusList.add(cArr[j] - '0');
+            char[] input = br.readLine().toCharArray();
+            List<Integer> status = new ArrayList<>();
+            for (int j = 0 ; j < input.length ; j++) {
+                status.add(input[j] - '0');
+            }
+            nodeArr[i] = new Node(i, status);
+        }
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0 ; i < n ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int direction = Integer.parseInt(st.nextToken());
+            nodeArr[start].start(direction);
+        }
+
+        int answer = 0;
+        for (int i = 1 ; i <= 4 ; i++) {
+            int result = nodeArr[i].getResult();
+            if (result == 1) {
+                answer += Math.pow(2, i - 1);
             }
         }
-        Total total = new Total(arr);
 
-        int k = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        for (int i = 0 ; i < k ; i++) {
-            st = new StringTokenizer(br.readLine());
-            int index = Integer.parseInt(st.nextToken());
-            int direction = Integer.parseInt(st.nextToken());
+        System.out.println(answer);
+    }
 
-            total.rotation(index, direction);
+    static class Node {
+        int num;
+        List<Integer> status = new ArrayList<>();
+
+        public Node (int num, List<Integer> status) {
+            this.num = num;
+            this.status = status;
         }
 
-        System.out.println(total.result());
+        public void start(int direction) {
+            left(num - 1, direction == 1 ? -1 : 1);
+            right(num + 1, direction == 1 ? -1 : 1);
+            rotaion(this.num, direction);
+        }
+
+        public void left(int num, int direction) {
+            if (num <= 0) return;
+            if (leftCheck(num)) return;
+            left(num - 1, direction == 1 ? -1 : 1);
+            rotaion(num, direction);
+        }
+
+        public boolean leftCheck(int cur) {
+            int now = nodeArr[cur].status.get(2);
+            int right = nodeArr[cur + 1].status.get(6);
+
+            if (right == now) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public void right(int num, int direction) {
+            if (num > 4) return;
+            if (rightCheck(num)) return;
+            right(num + 1, direction == 1 ? -1 : 1);
+            rotaion(num, direction);
+        }
+
+        public boolean rightCheck(int cur) {
+            int now = nodeArr[cur].status.get(6);
+            int left = nodeArr[cur - 1].status.get(2);
+
+            if (left == now) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public void rotaion(int num, int direction) {
+            List<Integer> status = nodeArr[num].status;
+            if (direction == -1) {
+                status.add(status.get(0));
+                status.remove(0);
+            } else {
+                status.add(0, status.get(status.size() - 1));
+                status.remove(status.size() - 1);
+            }
+        }
+
+
+        public int getResult() {
+            return status.get(0);
+        }
     }
+
 }
