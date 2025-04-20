503e1336cae518d23137dd35cdf72373fde8fe67
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-04-20 22:40:28 +0900

    [Gold IV] Title: 연구소 2, Time: 276 ms, Memory: 46632 KB -BaekjoonHub

--- 백준/Gold/17141. 연구소 2/연구소 2.java
+++ 백준/Gold/17141. 연구소 2/연구소 2.java
@@ -2,84 +2,7 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int n;
-    static int m;
-    static int[][] graph;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int min = Integer.MAX_VALUE;
-    static boolean flag = false;
-    static Point[] points;
-    static boolean[] pointVisited;
-
-    public static void BFS() {
-        int[][] dis = new int[n][n];
-        boolean[][] visited = new boolean[n][n];
-
-        Queue<int[]> q = new LinkedList<>();
-
-        // init
-        for (int i = 0 ; i < pointVisited.length ; i++) {
-            if (pointVisited[i] == true) {
-                q.offer(new int[]{points[i].x, points[i].y});
-                visited[points[i].x][points[i].y] = true;
-            }
-        }
-
-        while (!q.isEmpty()) {
-            int curX = q.peek()[0];
-            int curY = q.peek()[1];
-            q.poll();
-
-            for (int i = 0 ; i < 4 ; i++) {
-                int nx = curX + dx[i];
-                int ny = curY + dy[i];
-
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny] && (graph[nx][ny] == 0 || graph[nx][ny] == 2)) {
-                    visited[nx][ny] = true;
-                    dis[nx][ny] = dis[curX][curY] + 1;
-                    q.offer(new int[]{nx, ny});
-                }
-            }
-        }
-
-        int time = Integer.MIN_VALUE;
-
-        boolean full = true;
-        for (int i = 0 ; i < n ; i++) {
-            for (int j = 0 ; j < n ; j++) {
-                if ((graph[i][j] == 0 || graph[i][j] == 2) && visited[i][j] == false) {
-                    full = false;
-                }
-            }
-        }
-
-        if (full) {
-            flag = true;
-            for (int i = 0 ; i < n ; i++) {
-                for (int j = 0 ; j < n ; j++) {
-                    if (graph[i][j] == 0 || graph[i][j] == 2) {
-                        time = Math.max(dis[i][j], time);
-                    }
-                }
-            }
-            min = Math.min(time, min);
-        }
-    }
-
-    public static void DFS(int depth, int x) {
-        if (depth == m) {
-            BFS();
-        } else {
-            for (int i = x ; i < points.length ; i++) {
-                pointVisited[i] = true;
-                DFS(depth + 1, i + 1);
-                pointVisited[i] = false;
-            }
-        }
-    }
-
-    public static class Point {
+    static class Point {
         int x;
         int y;
 
@@ -89,39 +12,100 @@ class Main {
         }
     }
 
+    static int n;
+    static int m;
+    static int[][] map;
+    static List<Point> virusList = new ArrayList<Point>();
+    static int result = Integer.MAX_VALUE;
+    static Point[] select;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        graph = new int[n][n];
-        List<Point> pointList = new ArrayList<>();
+        map = new int[n][n];
+        select = new Point[m];
 
         for (int i = 0 ; i < n ; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0 ; j < n ; j++) {
-                int input = Integer.parseInt(st.nextToken());
-                graph[i][j] = input;
-                if (input == 2) {
-                    pointList.add(new Point(i, j));
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 2) {
+                    virusList.add(new Point(i, j));
                 }
             }
         }
 
-        points = new Point[pointList.size()];
-        pointVisited = new boolean[pointList.size()];
-
-        for (int i = 0 ; i < pointList.size() ; i++) {
-            points[i] = pointList.get(i);
-        }
-
         DFS(0, 0);
-
-        if (flag) {
-            System.out.println(min);
-        } else {
+        if (result == Integer.MAX_VALUE) {
             System.out.println(-1);
+        } else {
+            System.out.println(result);
         }
+
+    }
+
+    public static void DFS(int cnt, int start) {
+        if (cnt == m) {
+            BFS();
+        } else {
+            for (int i = start ; i < virusList.size() ; i++) {
+                Point virus = virusList.get(i);
+                select[cnt] = new Point(virus.x, virus.y);
+                DFS(cnt + 1, i + 1);
+            }
+        }
+    }
+
+    static int[][] direction = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    public static void BFS() {
+        int[][] copy = new int[n][n];
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < n ; j++) {
+                int cur = map[i][j];
+                if (cur == 0 || cur == 2) {
+                    copy[i][j] = -1;
+                } else {
+                    copy[i][j] = -2;
+                }
+            }
+        }
+        Queue<Point> q = new LinkedList<>();
+        boolean[][] visited = new boolean[n][n];
+
+        for (int i = 0 ; i < m ; i++) {
+            Point virus = select[i];
+            q.offer(virus);
+            visited[virus.x][virus.y] = true;
+            copy[virus.x][virus.y] = 0;
+        }
+
+        while(!q.isEmpty()) {
+            Point now = q.poll();
+            for (int i = 0 ; i < 4 ; i++) {
+                int nx = now.x + direction[i][0];
+                int ny = now.y + direction[i][1];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny] && copy[nx][ny] == -1) {
+                    visited[nx][ny] = true;
+                    copy[nx][ny] = copy[now.x][now.y] + 1;
+                    q.offer(new Point(nx, ny));
+                }
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < n ; j++) {
+                if (copy[i][j] == -1) {
+                    return;
+                }
+                max = Math.max(max, copy[i][j]);
+            }
+        }
+
+        result = Math.min(max, result);
     }
 }
