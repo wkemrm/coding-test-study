35ae962e92285a99bcb87b6a97a54dffd825539c
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-07-14 19:47:14 +0900

    [Gold IV] Title: N-Queen, Time: 4484 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,41 +1,44 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
+public class Main {
     static int n;
+    static int count = 0;
     static int[] arr;
-    static int count;
-    
-    public static void solution(int depth) {
-        if (depth == n) {
-            count++;
-        } else {
-            for (int i = 0 ; i < n ; i++) {
-                arr[depth] = i;
-                
-                if (possible(depth)) {
-                    solution(depth+1);
-                }
-            }
-        }
-    }
-    
-    public static boolean possible(int col) {
-        for (int i = 0 ; i < col ; i++) {
-            if (arr[col] == arr[i]) return false;
-            
-            if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) return false;
-        }
-        return true;
-    }
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
-        
-        solution(0);
-        
-        System.out.print(count);
+        DFS(0);
+
+        System.out.println(count);
     }
+
+    public static void DFS(int level) {
+        if (level == n) {
+            count++;
+        } else {
+            for (int i = 0 ; i < n ; i++) {
+                arr[level] = i;
+
+                if (possible(level)) {
+                    DFS(level + 1);
+                }
+            }
+        }
+    }
+
+    public static boolean possible(int depth) {
+        int x = depth;
+        int y = arr[depth];
+
+        for (int i = 0 ; i < depth ; i++) {
+            if (arr[i] == arr[depth]) return false;
+
+            if (x - y == i - arr[i] || x + y == i + arr[i]) return false;
+        }
+
+        return true;
+    }
+
 }
