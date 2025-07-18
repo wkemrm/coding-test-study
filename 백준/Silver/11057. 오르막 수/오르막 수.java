e8cb95bef0fbc48dddcce056c03852cff605dcfc
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-07-18 23:13:11 +0900

    [Silver I] Title: 오르막 수, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -4,25 +4,23 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        int[][] dp = new int[n + 1][10];
+        int[][] arr = new int[n + 1][10];
         for (int i = 0 ; i < 10 ; i++) {
-            dp[1][i] = i + 1;
+            arr[1][i] = 1;
         }
 
-        if (n == 1) {
-            System.out.print(dp[1][9]);
-            return;
-        }
-        for (int i = 2 ; i <= n ; i++) {
-            dp[i][0] = dp[i - 1][0];
-            for (int j = 1 ; j < 10 ; j++) {
-                dp[i][j] = ((dp[i - 1][j] % 10007) + (dp[i][j - 1] % 10007)) % 10007;
+        for (int now = 2 ; now <= n ; now++) {
+            arr[now][0] = 1;
+            for (int i = 1 ; i < 10 ; i++) {
+                arr[now][i] = (arr[now - 1][i] + arr[now][i - 1]) % 10007;
             }
         }
 
-
-        System.out.print(dp[n][9]);
+        int result = 0;
+        for (int i = 0 ; i < 10 ; i++) {
+            result = (result + arr[n][i]) % 10007;
+        }
+        System.out.print(result);
     }
 }
