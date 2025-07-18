f4e6b63d555a83aed33608bee949ecb54b43aea6
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-07-19 00:24:45 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -5,31 +5,24 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long[][] dp = new long[n + 1][10];
-        int mod = 1000000000;
-        
-        for (int i = 0 ; i <= 9 ; i++) {
-            dp[1][i] = 1;
+        int[][] arr = new int[n + 1][10];
+        for (int i = 0 ; i < 10 ; i++) {
+            arr[1][i] = 1;
         }
-        
-        for (int i = 2 ; i <= n ; i++) {
-            for (int j = 0; j <= 9 ; j++) {
-                if (j == 0) {
-                    dp[i][j] = dp[i - 1][1] % mod;
-                } else if(j == 9) {
-                    dp[i][j] = dp[i - 1][8] % mod;
-                } else {
-                    dp[i][j] = (dp[i - 1][j - 1] % mod + dp[i - 1][j + 1] % mod) % mod;
-                }
+
+        for (int now = 2 ; now <= n ; now++) {
+            arr[now][0] = arr[now - 1][1];
+            for (int i = 1 ; i < 9 ; i++) {
+                arr[now][i] = (arr[now - 1][i - 1] + arr[now - 1][i + 1]) % 1000000000;
             }
+            arr[now][9] = arr[now - 1][8];
         }
-        
+
         int result = 0;
-        for (int i = 1 ; i <= 9 ; i++) {
-            result += dp[n][i];
-            result %= mod;
+        for (int i = 1 ; i < 10 ; i++) {
+            result = (result + arr[n][i]) % 1000000000;
         }
-        
-        System.out.print(result % mod);
+
+        System.out.println(result);
     }
 }
