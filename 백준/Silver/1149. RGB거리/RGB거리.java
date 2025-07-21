ea6d4cefda0e195331ade4162fbf3a6de8eb6a1b
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-07-21 15:15:22 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -2,35 +2,30 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    static final int red = 0;
+    static final int blue = 1;
+    static final int green = 2;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n + 1][3];
         int[][] dp = new int[n + 1][3];
 
         for (int i = 1 ; i <= n ; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0 ; j < 3 ; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int i = 0; i < 3 ; i++) {
-            dp[1][i] = arr[1][i];
+
+        for (int i = 2; i <= n ; i++) {
+            dp[i][red] += Math.min(dp[i - 1][green], dp[i - 1][blue]);
+            dp[i][green] += Math.min(dp[i - 1][red], dp[i - 1][blue]);
+            dp[i][blue] += Math.min(dp[i - 1][red], dp[i - 1][green]);
         }
 
-        for (int i = 2 ; i <= n ; i++) {
-            for (int j = 0 ; j < 3 ; j++) {
-                int min = Integer.MAX_VALUE;
-                for (int k = 0; k < 3; k++) {
-                    if (k == j) continue;
-                    min = Math.min(min, dp[i - 1][k]);
-                }
-                dp[i][j] = min + arr[i][j];
-            }
-        }
-
-        int result = Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2]);
+        int result = Math.min(Math.min(dp[n][red], dp[n][blue]), dp[n][green]);
         System.out.println(result);
     }
 }
