96a4f600b7631ab3557c8b008cb72706232c6042
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-07-23 13:04:02 +0900

    [Silver I] Title: 카드 구매하기, Time: 124 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -5,19 +5,20 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] p = new int[n + 1];
+        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1 ; i <= n ; i++) {
-            p[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int[] dp = new int[n + 1];
-        
-        for (int i = 1 ; i <= n ; i++) {
-            for (int j = 0 ; j < i ; j++) {
-                dp[i] = Math.max(dp[j] + p[i - j], dp[i]);
+
+        dp[1] = arr[1];
+
+        for (int i = 2 ; i <= n ; i++) {
+            for (int j = i ; j >= 1 ; j--) {
+                dp[i] = Math.max(dp[i - j] + arr[j], dp[i]);
             }
         }
-        
-        System.out.print(dp[n]);
+        System.out.println(dp[n]);
     }
 }
