869109f151432a48dc6b4c6576e4945a2a01a58e
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-07-23 11:20:55 +0900

    [Gold III] Title: 내리막 길, Time: 332 ms, Memory: 37284 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -2,42 +2,49 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int[][] move = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     static int n;
     static int m;
-    static int[][] arr;
+    static int[][] board;
     static long[][] dis;
+    static boolean[][] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        arr = new int[n][m];
+        board = new int[n][m];
         dis = new long[n][m];
+        visited = new boolean[n][m];
+
         for (int i = 0 ; i < n ; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0 ; j < m ; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                dis[i][j] = -1;
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        DFS(0, 0);
+        long result = DFS(0, 0);
 
-        System.out.println(dis[0][0]);
+        System.out.println(result);
     }
 
+    static int[][] move = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
     public static long DFS(int x, int y) {
-        if (x == n - 1 && y == m - 1) return 1;
-        if (dis[x][y] != -1) return dis[x][y];
+        if (x == n - 1 && y == m - 1) {
+            return 1;
+        }
 
-        dis[x][y] = 0;
+        if (visited[x][y]) {
+            return dis[x][y];
+        }
 
+        visited[x][y] = true;
         for (int i = 0 ; i < 4 ; i++) {
             int nx = x + move[i][0];
             int ny = y + move[i][1];
-
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] < arr[x][y]) {
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[x][y] > board[nx][ny]) {
                 dis[x][y] += DFS(nx, ny);
             }
         }
