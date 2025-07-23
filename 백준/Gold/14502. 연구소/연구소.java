33d624bb9c711c6da95554384f49dbf219a36d93
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-07-23 17:13:33 +0900

    [Gold IV] Title: 연구소, Time: 808 ms, Memory: 297476 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,82 +1,85 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
     static int n;
     static int m;
-    static int[][] graph;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int result = Integer.MIN_VALUE;
+    static int[][] board;
+    static int max = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        graph = new int[n][m];
+        board = new int[n][m];
+
         for (int i = 0 ; i < n ; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0 ; j < m ; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         DFS(0);
-        System.out.print(result);
+        System.out.println(max);
     }
 
-    public static void DFS(int depth) {
-        if (depth == 3) {
-            BFS();
+    public static void DFS(int level) {
+        if (level == 3) {
+            solution();
         } else {
             for (int i = 0 ; i < n ; i++) {
-                for (int j = 0; j < m ; j++) {
-                    if (graph[i][j] == 0) {
-                        graph[i][j] = 1;
-                        DFS(depth + 1);
-                        graph[i][j] = 0;
-                    }
+                for (int j = 0 ; j < m ; j++) {
+                    if (board[i][j] != 0) continue;
+
+                    board[i][j] = 1;
+                    DFS(level + 1);
+                    board[i][j] = 0;
                 }
             }
         }
     }
 
-    public static void BFS() {
-        int[][] copyGraph = new int[n][m];
-        Queue<int[]> q = new LinkedList<int[]>();
+    static int[][] move = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    public static void solution() {
+        int[][] copy = new int[n][m];
+        Queue<int[]> q = new LinkedList<>();
 
         for (int i = 0 ; i < n ; i++) {
             for (int j = 0 ; j < m ; j++) {
-                copyGraph[i][j] = graph[i][j];
-                if (graph[i][j] == 2) {
+                copy[i][j] = board[i][j];
+                if (copy[i][j] == 2) {
                     q.offer(new int[]{i, j});
                 }
             }
         }
 
-
         while(!q.isEmpty()) {
-            int curX = q.peek()[0];
-            int curY = q.peek()[1];
-            q.poll();
+            int[] now = q.poll();
+            int dx = now[0];
+            int dy = now[1];
+
             for (int i = 0 ; i < 4 ; i++) {
-                int nx = curX + dx[i];
-                int ny = curY + dy[i];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && copyGraph[nx][ny] == 0) {
-                    copyGraph[nx][ny] = 2;
+                int nx = dx + move[i][0];
+                int ny = dy + move[i][1];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && copy[nx][ny] == 0) {
+                    copy[nx][ny] = 2;
                     q.offer(new int[]{nx, ny});
                 }
             }
         }
 
-        int count = 0;
+        int result = 0;
         for (int i = 0 ; i < n ; i++) {
             for (int j = 0 ; j < m ; j++) {
-                if (copyGraph[i][j] == 0) count++;
+                if (copy[i][j] == 0) {
+                    result++;
+                }
             }
         }
 
-        result = Math.max(result, count);
+        max = Math.max(result, max);
     }
 }
