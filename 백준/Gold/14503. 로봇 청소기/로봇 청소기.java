7e8f5ce8daae602197365eeab3d22a127d0243ba
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-07-23 18:19:03 +0900

    [Gold V] Title: 로봇 청소기, Time: 112 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -4,100 +4,97 @@ import java.io.*;
 class Main {
     static int n;
     static int m;
-    static int[][] map;
+    static int[][] board;
+    static boolean flag = true;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
+        board = new int[n][m];
         st = new StringTokenizer(br.readLine());
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
-        Robot robot = new Robot(r, c, d);
+        Robot robot = new Robot(
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()));
 
         for (int i = 0 ; i < n ; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0 ; j < m ; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        while(true) {
-            robot.nowClean();
-            if (robot.isEmptyByFour()) {
-                robot.empty();
+        while(flag) {
+            robot.clear();
+            if (robot.isMove()) {
+                robot.run();
             } else {
-                if (robot.notEmpty()) {
-                    break;
-                }
+                flag = robot.back();
             }
         }
 
-        System.out.println(robot.cnt);
+        System.out.print(robot.count);
     }
 
-    static int[][] direction = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    static int[][] move = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    static int[] left = {3, 0, 1, 2};
+    static int[] behind = {2, 3, 0, 1};
 
     static class Robot {
         int x;
         int y;
-        int cnt;
-        int d;
+        int direction;
+        int count;
 
-        public Robot(int x, int y, int d) {
+        public Robot(int x, int y, int direction) {
             this.x = x;
             this.y = y;
-            this.d = d;
-            this.cnt = 0;
+            this.direction = direction;
+            this.count = 0;
         }
 
-        public void nowClean() {
-            if (map[x][y] == 0) {
-                cnt++;
-                map[x][y] = -1;
+        public boolean isMove() {
+            for (int i = 0 ; i < 4 ; i++) {
+                int nx = this.x + move[i][0];
+                int ny = this.y + move[i][1];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
+                    return true;
+                }
             }
-        }
-
-        int[] behind = {2, 3, 0, 1};
-        // true인 경우 작동 멈춤
-        public boolean notEmpty() {
-            int nextBehind = behind[d];
-            int nx = x + direction[nextBehind][0];
-            int ny = y + direction[nextBehind][1];
-            // 후진 못하는 경우
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m || map[nx][ny] == 1) return true;
-
-            this.x = nx;
-            this.y = ny;
             return false;
         }
 
-        int[] left = {3, 0, 1, 2};
-        public void empty() {
-            int nextLeft = left[d];
-            int nx = x + direction[nextLeft][0];
-            int ny = y + direction[nextLeft][1];
-            d = nextLeft;
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == 0) {
+        public boolean back() {
+            int back = behind[direction];
+            int nx = x + move[back][0];
+            int ny = y + move[back][1];
+            if (nx < 0 || nx >= n && ny < 0 && ny >= m || board[nx][ny] == 1) {
+                return false;
+            }
+
+            this.x = nx;
+            this.y = ny;
+            return true;
+        }
+
+        public void run() {
+            this.direction = left[direction];
+            int nx = this.x + move[this.direction][0];
+            int ny = this.y + move[this.direction][1];
+
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
                 this.x = nx;
                 this.y = ny;
             }
         }
 
-        public boolean isEmptyByFour() {
-            boolean flag = false;
-            for (int i = 0 ; i < 4 ; i++) {
-                int nx = x + direction[i][0];
-                int ny = y + direction[i][1];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == 0) {
-                    flag = true;
-                    break;
-                }
+        public void clear() {
+            if (board[this.x][this.y] == 0) {
+                this.count++;
+                board[this.x][this.y] = -1;
             }
-            return flag;
         }
     }
 }
