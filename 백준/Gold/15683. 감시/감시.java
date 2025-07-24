4541985fcaee7dff023c2e8b3309345bcf8d4833
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-07-24 17:12:10 +0900

    [Gold III] Title: 감시, Time: 308 ms, Memory: 48168 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -4,8 +4,10 @@ import java.io.*;
 class Main {
     static int n;
     static int m;
-    static List<Cctv> cctvLocation = new ArrayList<>();
-    static int[][] map;
+    static int[][] board;
+    static Direction[] directions;
+    static Cctv[] cctv;
+    static int count;
     static int result = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
@@ -13,128 +15,122 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        directions = new Direction[6];
+        directions[1] = new Direction(new int[][]{{0}, {1}, {2}, {3}});
+        directions[2] = new Direction(new int[][]{{0, 2}, {1, 3}});
+        directions[3] = new Direction(new int[][]{{0, 1}, {1, 2}, {2, 3}, {3, 0}});
+        directions[4] = new Direction(new int[][]{{0, 1, 2}, {1, 2, 3}, {2, 3, 0}, {3, 0, 1}});
+        directions[5] = new Direction(new int[][]{{0, 1, 2, 3}});
 
-        map = new int[n][m];
-
+        count = 0;
         for (int i = 0 ; i < n ; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0 ; j < m ; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-
-                if (map[i][j] >= 1 && map[i][j] <= 5) {
-                   cctvLocation.add(new Cctv(i, j, map[i][j]));
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if (board[i][j] != 0 && board[i][j] != 6) {
+                    count++;
                 }
             }
         }
 
-        DFS(0);
+        cctv = new Cctv[count];
+        int now = 0;
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < m ; j++) {
+                if (board[i][j] == 0 || board[i][j] == 6) continue;
+                cctv[now] = new Cctv(board[i][j], i, j);
+                now++;
+            }
+        }
 
+        DFS(0, new int[count]);
         System.out.println(result);
     }
 
+    public static void DFS(int level, int[] arr) {
+        if (level == count) {
+            solution(arr);
+        } else {
+            Cctv now = cctv[level];
+            int[][] direction = directions[now.num].direction;
+            for (int i = 0 ; i < direction.length ; i++) {
+                arr[level] = i;
+                DFS(level + 1, arr);
+            }
+        }
+    }
+
+    public static void solution(int[] arr) {
+        int[][] copy = new int[n][m];
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < m ; j++) {
+                copy[i][j] = board[i][j];
+            }
+        }
+
+        for (int i = 0 ; i < arr.length ; i++) {
+            Cctv now = cctv[i];
+            int[][] direction = directions[now.num].direction;
+            int[] move = direction[arr[i]];
+            for (int j = 0 ; j < move.length ; j++) {
+                if (move[j] == 0) {
+                    for (int k = now.x - 1 ; k >= 0 ; k--) {
+                        if (copy[k][now.y] == 6) break;
+                        if (copy[k][now.y] == 0) {
+                            copy[k][now.y] = -1;
+                        }
+                    }
+                } else if (move[j] == 1) {
+                    for (int k = now.y + 1 ; k < m ; k++) {
+                        if (copy[now.x][k] == 6) break;
+                        if (copy[now.x][k] == 0) {
+                            copy[now.x][k] = -1;
+                        }
+                    }
+                } else if (move[j] == 2) {
+                    for (int k = now.x + 1 ; k < n ; k++) {
+                        if (copy[k][now.y] == 6) break;
+                        if (copy[k][now.y] == 0) {
+                            copy[k][now.y] = -1;
+                        }
+                    }
+                } else {
+                    for (int k = now.y - 1 ; k >= 0 ; k--) {
+                        if (copy[now.x][k] == 6) break;
+                        if (copy[now.x][k] == 0) {
+                            copy[now.x][k] = -1;
+                        }
+                    }
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < m ; j++) {
+                if (copy[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+        result = Math.min(count, result);
+    }
+
+    static class Direction {
+        int[][] direction;
+        public Direction (int[][] direction) {
+            this.direction = direction;
+        }
+    }
+
     static class Cctv {
+        int num;
         int x;
         int y;
-        int num;
-
-        public Cctv(int x, int y, int num) {
+        public Cctv(int num, int x, int y) {
+            this.num = num;
             this.x = x;
             this.y = y;
-            this.num = num;
         }
-
-        public List<char[]> getDirection() {
-            switch(num) {
-                case 1 : {
-                    return List.of(new char[]{'R'}, new char[]{'D'}, new char[]{'L'}, new char[]{'U'});
-                }
-                case 2: {
-                    return List.of(new char[]{'L', 'R'}, new char[]{'U', 'D'});
-                }
-                case 3 : {
-                    return List.of(new char[]{'U', 'R'}, new char[]{'R', 'D'}, new char[]{'D', 'L'}, new char[]{'L', 'U'});
-                }
-                case 4 : {
-                    return List.of(new char[]{'L', 'U', 'R'}, new char[]{'U', 'R', 'D'}, new char[]{'R', 'D', 'L'}, new char[]{'D', 'L', 'U'});
-                }
-                case 5 : {
-                    return List.of(new char[]{'L', 'U', 'R', 'D'});
-                }
-                default: {
-
-                }
-            }
-            return null;
-        }
-    }
-
-    static List<char[]> selectCctv = new ArrayList<>();
-
-    public static void DFS(int depth) {
-        if (depth == cctvLocation.size()) {
-            cal();
-        } else {
-            Cctv cctv = cctvLocation.get(depth);
-            for (char[] direction : cctv.getDirection()) {
-                selectCctv.add(direction);
-                DFS(depth + 1);
-                selectCctv.remove(selectCctv.size() - 1);
-            }
-        }
-    }
-
-    public static void cal() {
-        int[][] copyMap = new int[n][m];
-
-        for (int i = 0 ; i < n ; i++) {
-            for (int j = 0 ; j < m ; j++) {
-                copyMap[i][j] = map[i][j];
-            }
-        }
-
-        for (int j = 0 ; j < selectCctv.size() ; j++) {
-            Cctv cctv = cctvLocation.get(j);
-            for (int i = 0 ; i < selectCctv.get(j).length ; i++) {
-                switch (selectCctv.get(j)[i]) {
-                    case 'L' : {
-                        for (int target = cctv.y - 1 ; target >= 0 ; target--) {
-                            if (copyMap[cctv.x][target] == 6) break;
-                            copyMap[cctv.x][target] = -1;
-                        }
-                        break;
-                    }
-                    case 'R' : {
-                        for (int target = cctv.y + 1 ; target < m ; target++) {
-                            if (copyMap[cctv.x][target] == 6) break;;
-                            copyMap[cctv.x][target] = -1;
-                        }
-                        break;
-                    }
-                    case 'U' : {
-                        for (int target = cctv.x - 1 ; target >= 0 ; target--) {
-                            if (copyMap[target][cctv.y] == 6) break;
-                            copyMap[target][cctv.y] = -1;
-                        }
-                        break;
-                    }
-                    case 'D' : {
-                        for (int target = cctv.x + 1 ; target < n ; target++) {
-                            if (copyMap[target][cctv.y] == 6) break;
-                            copyMap[target][cctv.y] = -1;
-                        }
-                        break;
-                    }
-                }
-            }
-        }
-
-        int cnt = 0;
-        for (int i = 0 ; i < n ; i++) {
-            for (int j = 0 ; j < m ; j++) {
-                if (copyMap[i][j] == 0) cnt++;
-            }
-        }
-
-        result = Math.min(result, cnt);
     }
 }
