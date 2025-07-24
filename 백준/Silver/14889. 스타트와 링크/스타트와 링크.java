6a6308ddadb9e3394c3f96c416dd452abbe01a50
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-07-24 13:03:54 +0900

    [Silver I] Title: 스타트와 링크, Time: 324 ms, Memory: 25364 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -3,53 +3,58 @@ import java.io.*;
 
 class Main {
     static int n;
-    static boolean[] visited;
-    static int[][] graph;
-    static int result = Integer.MAX_VALUE;
-
-    public static void combi(int depth, int before) {
-        if (depth == n / 2) {
-            diff();
-        } else {
-            for (int i = before ; i <= n ; i++) {
-                visited[i] = true;
-                combi(depth + 1, i + 1);
-                visited[i] = false;
-            }
-        }
-    }
-
-    public static void diff() {
-        int startSum = 0;
-        int linkSum = 0;
-
-        for (int i = 1 ; i < n ; i++) {
-            for (int j = i + 1 ; j <= n ; j++) {
-                if (visited[i] == true && visited[j] == true) {
-                    startSum += graph[i - 1][j - 1] + graph[j - 1][i - 1];
-                } else if (visited[i] == false && visited[j] == false) {
-                    linkSum += graph[i - 1][j - 1] + graph[j - 1][i - 1];
-                }
-            }
-        }
-        result = Math.min(result, Math.abs(startSum - linkSum));
-    }
+    static int[][] arr;
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1][n + 1];
 
-        graph = new int[n][n];
-        visited = new boolean[n + 1];
-
-        for (int i = 0 ; i < n ; i++) {
+        for (int i = 1 ; i <= n ; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0 ; j < n ; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1 ; j <= n ; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        combi(0, 1);
-        System.out.print(result);
+        DFS(0, 1, new int[n / 2]);
+        System.out.println(min);
+    }
+
+    public static void DFS(int level, int x, int[] start) {
+        if (level == n / 2) {
+            solution(start);
+        } else {
+            for (int i = x ; i <= n ; i++) {
+                start[level] = i;
+                DFS(level + 1, i + 1, start);
+            }
+        }
+    }
+
+    public static void solution(int[] start) {
+        boolean[] visited = new boolean[n + 1];
+
+        int startSum = 0;
+        for (int i = 0 ; i < start.length ; i++) {
+            visited[start[i]] = true;
+            for (int j = 0 ; j < start.length ; j++) {
+                if (i == j) continue;
+                startSum += arr[start[i]][start[j]];
+            }
+        }
+
+        int linkSum = 0;
+        for (int i = 1 ; i <= n ; i++) {
+            if (visited[i]) continue;
+            for (int j = 1 ; j <= n ; j++) {
+                if (visited[j]) continue;
+                if (i == j) continue;
+                linkSum += arr[i][j];
+            }
+        }
+
+        min = Math.min(Math.abs(startSum - linkSum), min);
     }
 }
