8a722de7aa3548d998752d7a42e3a3ea45ff59ae
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-10-17 21:19:58 +0900

    [Gold III] Title: 파티, Time: 512 ms, Memory: 76972 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,29 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-class Node implements Comparable<Node> {
-    int index;
-    int cost;
-    public Node(int index, int cost) {
-        this.index = index;
-        this.cost = cost;
-    }
-
-    public int compareTo(Node node) {
-        return this.cost - node.cost;
-    }
-
-}
 class Main {
-    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+    static int n;
+    static int m;
+    static int x;
+    static List<List<Edge>> graph = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int x = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+
         for (int i = 0 ; i <= n ; i++) {
             graph.add(new ArrayList<>());
         }
@@ -33,43 +23,61 @@ class Main {
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            graph.get(start).add(new Node(end, cost));
+            graph.get(start).add(new Edge(end, cost));
         }
 
-        int[] returnDist = solution(n, x);
-
-        int result = Integer.MIN_VALUE;
-        for (int i = 1 ; i <= n ; i++) {
-            int[] goingDist = solution(n, i);
-            result = Math.max(goingDist[x] + returnDist[i], result);
-        }
-
-        System.out.print(result);
+        System.out.print(dick());
     }
 
-    public static int[] solution(int n, int start) {
-        boolean[] check = new boolean[n + 1];
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
+    public static int dick() {
+        int[][] dis = new int[n + 1][n + 1];
+        for (int i = 1 ; i <= n ; i++) {
+            Arrays.fill(dis[i], Integer.MAX_VALUE);
+        }
 
-        while(!pq.isEmpty()) {
-            Node node = pq.poll();
-            int now = node.index;
+        for (int i = 1 ; i <= n ; i++) {
+            dis[i][i] = 0;
+            PriorityQueue<Edge> pq = new PriorityQueue<>();
 
-            if (check[now]) continue;
-            check[now] = true;
+            pq.offer(new Edge(i, 0));
 
-            for (Node next : graph.get(now)) {
-                if (dist[next.index] > dist[now] + next.cost) {
-                    dist[next.index] = dist[now] + next.cost;
-                    pq.offer(new Node(next.index, dist[next.index]));
+            while(!pq.isEmpty()) {
+                Edge nowEdge = pq.poll();
+                int nowCost = nowEdge.cost;
+                int now = nowEdge.end;
+
+                if (dis[i][now] < nowCost) {
+                    continue;
+                }
+
+                for (Edge edge : graph.get(now)) {
+                    if (dis[i][edge.end] > edge.cost + nowCost) {
+                        dis[i][edge.end] = edge.cost + nowCost;
+                        pq.offer(new Edge(edge.end, edge.cost + nowCost));
+                    }
                 }
             }
         }
 
-        return dist;
+        int max = Integer.MIN_VALUE;
+        for (int i = 1 ; i <= n ; i++) {
+            max = Math.max(max, dis[i][x] + dis[x][i]);
+        }
+
+        return max;
+    }
+
+    static class Edge implements Comparable<Edge>{
+        int end;
+        int cost;
+
+        public Edge(int end, int cost) {
+            this.end = end;
+            this.cost = cost;
+        }
+
+        public int compareTo(Edge edge) {
+            return this.cost - edge.cost;
+        }
     }
 }
