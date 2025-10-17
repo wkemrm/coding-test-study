93098f124a4020ac20fbca8977b29c2d2a9e21ad
이동연 <63217462+wkemrm@users.noreply.github.com>
2025-10-17 14:06:03 +0900

    [Gold V] Title: 집합의 표현, Time: 960 ms, Memory: 54040 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -11,22 +11,23 @@ class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         arr = new int[n + 1];
+        
         for (int i = 0 ; i <= n ; i++) {
             arr[i] = i;
         }
         
         for (int i = 0 ; i < m ; i++) {
             st = new StringTokenizer(br.readLine());
-            int input = Integer.parseInt(st.nextToken());
+            int cal = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             
-            if (input == 0) {
+            if (cal == 0) {
                 union(a, b);
             } else {
-                int root1 = find(a);
-                int root2 = find(b);
-                if (root1 == root2) {
+                int fa = find(a);
+                int fb = find(b);
+                if (fa == fb) {
                     System.out.println("YES");
                 } else {
                     System.out.println("NO");
@@ -35,18 +36,21 @@ class Main {
         }
     }
     
-    public static int find(int num) {
-        if (num == arr[num]) {
-            return num;
+    public static int find(int input) {
+        if (arr[input] == input) {
+            return input;
         }
-        
-        return find(arr[num]);
+        return arr[input] = find(arr[input]);
     }
     
-    public static void union(int first, int second) {
-        first = find(first);
-        second = find(second);
+    public static void union(int a, int b) {
+        int fa = find(a);
+        int fb = find(b);
         
-        arr[second] = first;
+        if (fa < fb) {
+            arr[fb] = fa;
+        } else {
+            arr[fa] = fb;
+        }
     }
 }
